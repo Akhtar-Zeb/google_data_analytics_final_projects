8d8d7be4bd8d627194729cbb9a0afe2e8a3341f0
Akhtar Zeb <akhtarzebtmg@gmail.com>
2025-11-05 19:03:53 +0500

    Add text_cleaner.py file a Two function customers and products

--- text_cleaner.py
+++ text_cleaner.py
@@ -3,10 +3,12 @@ import re
 import csv
 
 def main():
-
+    # Ask for file path
     input_file = input("Enter file path: ")
+    # Split file path and name
     parts = input_file.split("/")
     file_path, file_name = parts
+    # Check name and send it to specific function
     if file_name == "CUST.txt":
         customers(input_file)
     elif file_name == "PROD.txt":
@@ -23,30 +25,68 @@ def main():
         print("File not found.")
 
         
-
+# Clean customer(CUST.txt) text file
 def customers(inputfile):
-    take_name = inputfile.split(".")
-    second_parts = take_name[0].split("/")
-    output_file = f"clean_data/{second_parts[1].lower()}_clean.csv"
+    # Save output file to clean_data folder
+    output_file = f"clean_data/customers_clean.csv"
 
+    # Create Empty list for clean data
     clean_data = []
+    # Open file with read mode
     with open(inputfile, "r") as infile:
+        # Perse file line by line
         for line in infile:
+            # Split line by two are more space
             parts = re.split(r'\s{2,}', line.strip())
 
+            # append parts to list
             clean_data.append(parts)
-
+    
+    # Open file for write mode
     with open(output_file, "w") as outfile:
         writer = csv.writer(outfile)
+        # Give file a Header row
         writer.writerow(["customer_id", "customer_name", "customer_location"])
+
+        # Parse clean data list
         for line in clean_data:
+            # check if line is greater or equal to 3 parts if it is only write three parts
             if len(line) >= 3:
                 writer.writerow(line[:3])
+            # check if line equal to two then add empty field
             elif len(line) == 2:
                 writer.writerow(line + [""])
 
 def products(inputfile):
-    pass
+    # Save output file to clean_data folder
+    output_file = f"clean_data/products_clean.csv"
+
+    # Create Empty list for clean data
+    clean_data = []
+    # Open file with read mode
+    with open(inputfile, "r") as infile:
+        # Perse file line by line
+        for line in infile:
+            # Split line by two are more space
+            parts = re.split(r'\s{2,}', line.strip())
+
+            # append parts to list
+            clean_data.append(parts)
+    
+    # Open file for write mode
+    with open(output_file, "w") as outfile:
+        writer = csv.writer(outfile)
+        # Give file a Header row
+        writer.writerow(["product_id", "product_name", "product_rate"])
+
+        # Parse clean data list
+        for line in clean_data:
+            # check if line is greater or equal to 3 parts if it is only write three parts
+            if len(line) >= 3:
+                writer.writerow(line[:3])
+            # check if line equal to two then add empty field
+            elif len(line) == 2:
+                writer.writerow(line + [""])
 
 def transactions(inputfile):
     pass
